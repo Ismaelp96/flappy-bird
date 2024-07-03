a25d40048c8bd86c66387635d92a3e173ad69dfc
ismaelp96 <ismael6991@live.com>
2024-07-03 18:33:22 -0300

    att index do core

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,5 +1,7 @@
+import CicloObstaculos from "./obstaculo/CicloObstaculos"
+import { EspacoObstaculos } from "./obstaculo/EspacoObstaculos"
 import { Nivel } from "./obstaculo/Nivel"
 import Obstaculo from "./obstaculo/Obstaculo"
 import Passaro from "./passaro/Passaro"
 
-export { Nivel, Passaro, Obstaculo }
+export { Nivel, Passaro, Obstaculo, CicloObstaculos, EspacoObstaculos }
