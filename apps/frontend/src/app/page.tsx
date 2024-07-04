18001872303c830c9173d5c77cc5ca33a2c6398e
ismaelp96 <ismael6991@live.com>
2024-07-04 16:08:49 -0300

    adicionando os menus ao page.tsx

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,6 +1,10 @@
 "use client"
 import Canos from "@/components/jogo/Canos"
 import Passaro from "@/components/jogo/Passaro"
+import Comandos from "@/components/menu/Comando"
+import EspacoObstaculos from "@/components/menu/EspacoObstaculos"
+import NiveisDiculdade from "@/components/menu/NiveisDiculdade"
+import VelocidadeJogo from "@/components/menu/VelocidadeJogo"
 import useJogo from "@/data/hook/useJogo"
 
 export default function Home() {
@@ -12,6 +16,12 @@ export default function Home() {
                 <span className="text-red-400">Flappy</span>
                 <span className="text-green-400">Bird</span>
             </h1>
+            <div className="flex p-5 justify-between gap-5">
+                <VelocidadeJogo />
+                <NiveisDiculdade />
+                <EspacoObstaculos />
+                <Comandos />
+            </div>
             <div className="flex w-4/6 h-4/6 relative overflow-hidden bg-background bg-no-repeat bg-cover bg-blue-400 border-4 border-blue-500">
                 <Passaro />
                 {obstaculos.itens.map((obstaculo, i) => (
