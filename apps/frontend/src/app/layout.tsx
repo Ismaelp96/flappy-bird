7822e22da7d5332f9d56ae66d2025cdba241deb3
ismaelp96 <ismael6991@live.com>
2024-07-04 12:04:08 -0300

    components do jogo e data ref. ao jogo

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,22 +1,18 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next"
+import { VT323 } from "next/font/google"
+import "./globals.css"
 
-const inter = Inter({ subsets: ['latin'] })
+const vt323 = VT323({ weight: "400", subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: 'Flappy Bird Limpo',
-  description: 'Flappy Bird com Arquitetura Limpa',
+    title: "Flappy Bird",
+    description: "Flappy Bird com Arquitetura Limpa",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className={vt323.className}>{children}</body>
+        </html>
+    )
 }
--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,7 +1,15 @@
+import Passaro from "@/components/jogo/Passaro"
+
 export default function Home() {
     return (
         <main className="flex flex-col justify-center items-center h-screen">
-            <h1 className="font-black text-3xl text-zinc-400">Usando Core</h1>
+            <h1 className="text-5xl space-x-4">
+                <span className="text-red-400">Flappy</span>
+                <span className="text-green-400">Bird</span>
+            </h1>
+            <div className="flex w-4/6 h-4/6 relative overflow-hidden bg-background bg-no-repeat bg-cover bg-blue-400 border-4 border-blue-500">
+                <Passaro />
+            </div>
         </main>
     )
 }
